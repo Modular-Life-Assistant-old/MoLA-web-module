576fc6abfbec3eabfe45cad899a2b91a0322f741
Treemo <treemo@hotmail.fr>
2015-12-06 01:36:04 +0100

    import

---
+++ Module.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+from core import DataFileManager
+
+from helpers.modules.NotificationModule import NotificationModule
+
+
+class Module(NotificationModule):
+    flask = Flask(__name__)
+    app_list = []
+
+    def add_blueprint(self, blueprint, icon='', **options):
+        self.flask.register_blueprint(blueprint, **options)
+        self.app_list.append({
+            'css_class': 'ui-btn-icon-left ui-icon-%s' % icon if icon else '',
+            'name': blueprint.name,
+            'url': blueprint.url_prefix,
+        })
+
+    def init(self):
+        self.flask.add_url_rule('/', view_func=self._index)
+        self.flask.add_url_rule('/home', 'home', view_func=self._home)
+
+    def run(self):
+        config = DataFileManager.load(self.name, 'config', {
+            'host' : '0.0.0.0',
+            'debug': False,
+        })
+        self.flask.run(**config)
+
+    def send(self, msg, image=None, sound=None):
+        pass
+
+    def _home(self):
+        return render_template('home.html', apps=self.app_list)
+
+    def _index(self):
+        return render_template('index.html', apps=self.app_list,
+                               content_url='/home')
